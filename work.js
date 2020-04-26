658545fe040f0caacead90b1f3416b867d4b0de7
awelshams <63799809+awelshams@users.noreply.github.com>
2020-04-26 14:43:56 +0100

    this is my work on javascript

---
+++ work.js
@@ -0,0 +1,33 @@
+// function greeting(name){
+//     window.alert("Hello "+ name);
+//     window.alert("Welcome to my website " + name);
+//     return personNAME;
+// };
+
+// var personNAME = "Emmanuel"
+
+// function greeting(name){
+//     var result = name + greeting;
+//     return result;
+// }
+
+// var addResult = addName
+
+ window.alert ("welcome to my site Amigo👏👏");
+ window.alert ("It is yours trully shams aka.shamooooo😛😋                                           aka.deenos🤙🤙");
+//  window.alert ("aka.deenos👌👌👌");
+ 
+
+ 
+ function changeImage(){
+    document.getElementById("img-change").src = "images/data-scientist.png";
+  }
+
+  function changeBackground(){
+    document.getElementById("demo").style.background = "linear-gradient(yellow,red,orange)";
+  }
+
+  function changeDestination(){
+    document.getElementsByTagName("a")[0].innerHTML = "http://jobberman.com";
+  }
+  
